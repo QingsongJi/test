27cdfbf1e84e3169c605c6cb168573c8ff47f8c9
QingsongJi <2542833497@qq.com>
2020-07-24 17:24:12 +0800

    try again

---
+++ sp01-jay-common/src/main/java/com/jay/sp01/service/ItemService.java
@@ -0,0 +1,13 @@
+package com.jay.sp01.service;
+
+import java.util.List;
+
+import com.jay.sp01.pojo.Item;
+
+public interface ItemService {
+
+	List<Item> getItems(String orderId);
+	
+	void decreaseNumbers(List<Item> list);
+
+}
